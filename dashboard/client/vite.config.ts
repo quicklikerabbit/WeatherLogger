75766f6d868590f798222e5a3e23b8562ea06346
quicklikerabbit <>
2026-08-23 22:37:04 -0700

    Make dev command accessible on the local network

--- dashboard/client/vite.config.ts
+++ dashboard/client/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true,
     proxy: {
       '/api': 'http://localhost:4000',
     },
